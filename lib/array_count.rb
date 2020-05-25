0ba5398fcd835da771aa19e490be703591eeb6e5
Christina Chianelli <cechianelli@gmail.com>
2020-05-25 03:11:09 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,6 +1,7 @@
 
 def count_strings(array)
   # Return the total number of strings in the provided array using the count enumerable
+  
   array.count { |element|
     element.class == String
   }
@@ -8,6 +9,7 @@ end
 
 def count_empty_strings(array)
   # Return the total number of EMPTY strings in the provided array using the count enumerable
+  
   array.count { |element|
     element == ""
   }
